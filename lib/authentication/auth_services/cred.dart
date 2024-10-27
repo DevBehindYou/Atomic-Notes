aab5e1b444bdb00dd6f637671889b9fdae0f4a1c
DevBehindYou <147663456+DevBehindYou@users.noreply.github.com>
2024-10-27 17:59:56 +0530

    d

--- lib/authentication/auth_services/cred.dart
+++ lib/authentication/auth_services/cred.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
 class CredService {
-  final String PROJECT_URL = "https://ytektrlqgyxhmipqshph.supabase.co";
+  final String PROJECT_URL = "https://fgwebsvzzxcrosyowbni.supabase.co";
   final String API_KEY =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0ZWt0cmxxZ3l4aG1pcHFzaHBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxNTI5NTksImV4cCI6MjAxNTcyODk1OX0.jpgbd1bsqla8BmKfcfwSeoa40j1xhl2TpW96BuXRkqE";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnd2Vic3Z6enhjcm9zeW93Ym5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3Nzk4MzEsImV4cCI6MjA0NTM1NTgzMX0.aR1Q6LWVwtUwz561S9v3UJSaf5JX6RhUrVCzsvvl-PM";
 }
