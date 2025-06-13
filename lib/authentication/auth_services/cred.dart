545be3443b872240e22d02665dd06da7a594b2ce
DevBehindYou <ashutoshsept20@gmail.com>
2025-06-14 00:06:14 +0530

    Update cred.dart

--- lib/authentication/auth_services/cred.dart
+++ lib/authentication/auth_services/cred.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: non_constant_identifier_names
 
 class CredService {
-  final String PROJECT_URL = "https://fgwebsvzzxcrosyowbni.supabase.co";
+  final String PROJECT_URL = "";
   final String API_KEY =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnd2Vic3Z6enhjcm9zeW93Ym5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3Nzk4MzEsImV4cCI6MjA0NTM1NTgzMX0.aR1Q6LWVwtUwz561S9v3UJSaf5JX6RhUrVCzsvvl-PM";
+      "";
 }
